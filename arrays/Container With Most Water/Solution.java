ff0558a4f5a60a162faf79ee996f7e288e86dee1
ywccc <38544085+ywccc@users.noreply.github.com>
2018-04-19 17:23:02 -0700

    Add files via upload

---
+++ arrays/Container With Most Water/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length == 0) return 0;
+        int l = 0;
+        int r = height.length-1;
+       
+        int res = 0;
+        
+        while(l < r)
+        {
+            res = Math.max(res,(r-l)*Math.min(height[l],height[r]));
+            if(height[l] < height[r])
+            {
+                ++l;
+            }
+            else --r;
+        }
+        
+        return res;
+    }
+}
