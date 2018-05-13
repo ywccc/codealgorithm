444b0c6e2af564a138fd9795efec5e0ea45d6ce9
ywccc <38544085+ywccc@users.noreply.github.com>
2018-05-12 22:46:38 -0700

    Create Solution.java

---
+++ arrays/Find the Celebrity/Solution.java
@@ -0,0 +1,21 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int res = 0;
+        for(int i = 1; i < n; ++i)
+        {
+            if(knows(res,i))
+            {
+                res = i;
+            }
+        }
+        for(int i = 0; i < n; ++i)
+        {
+            if(i != res && (!knows(i,res) || knows(res,i))) return -1;
+        }
+        
+        return res;
+    }
+}
